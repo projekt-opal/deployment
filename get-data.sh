e3ac00fd494106aa63ba43f5b51e0723def56533
Adrian Wilke <adibaba@users.noreply.github.com>
2021-07-06 12:38:12 +0200

    get data script

---
+++ get-data.sh
@@ -0,0 +1,9 @@
+wget https://hobbitdata.informatik.uni-leipzig.de/OPAL/OpalGraph/2020-11-18/2020-10/mdm-1.ttl.tar.gz
+wget https://hobbitdata.informatik.uni-leipzig.de/OPAL/OpalGraph/2020-11-18/2020-10/mcloud-1.ttl.tar.gz
+wget https://hobbitdata.informatik.uni-leipzig.de/OPAL/OpalGraph/2020-11-18/2020-10/govdata.tar.gz
+wget https://hobbitdata.informatik.uni-leipzig.de/OPAL/OpalGraph/2020-11-18/2020-10/edp.tar.gz
+
+tar -xzf mdm-1.ttl.tar.gz
+tar -xzf mcloud-1.ttl.tar.gz
+tar -xzf govdata.tar.gz
+tar -xzf edp.tar.gz
