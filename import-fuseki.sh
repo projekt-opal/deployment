336be48ba0a35e93589ad88f387550a18a6a2843
Adrian Wilke <adibaba@users.noreply.github.com>
2021-07-08 22:26:05 +0200

    fuseki import script

--- import-fuseki.sh
+++ import-fuseki.sh
@@ -6,6 +6,8 @@ TTL_NEW="/tmp/opal-data/opal-2020-10/*.ttl"
 API_OLD="http://localhost:3030/2020-06/data"
 API_NEW="http://localhost:3030/2020-10/data"
 
+date
+
 for FILE in $TTL_OLD
 do
   echo "$FILE"
@@ -17,3 +19,5 @@ do
   echo "$FILE"
   curl --location --request POST $API_NEW --header 'Content-Type: multipart/form-data' --form "file.ttl=@${FILE}"
 done
+
+date
